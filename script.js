63513457417c68da3c4b573ce0bb628e2329b50e
DiegoSandoval31 <115035210+DiegoSandoval31@users.noreply.github.com>
2024-02-27 13:21:15 -0300

    cambio idioma a español

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ function addEntry() {
   const targetInputContainer = document.querySelector(`#${entryDropdown.value} .input-container`);
   const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length + 1;
   const HTMLString = `
-  <label for="${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name</label>
-  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
-  <label for="${entryDropdown.value}-${entryNumber}-calories">Entry ${entryNumber} Calories</label>
+  <label for="${entryDropdown.value}-${entryNumber}-name">Ingrese Nombre ${entryNumber} </label>
+  <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Nombre" />
+  <label for="${entryDropdown.value}-${entryNumber}-calories">Ingrese Calorias ${entryNumber} </label>
   <input
     type="number"
     min="0"
     id="${entryDropdown.value}-${entryNumber}-calories"
-    placeholder="Calories"
+    placeholder="Calorias"
   />`;
   targetInputContainer.insertAdjacentHTML('beforeend', HTMLString);
 }
@@ -55,13 +55,13 @@ function calculateCalories(e) {
 
   const consumedCalories = breakfastCalories + lunchCalories + dinnerCalories + snacksCalories;
   const remainingCalories = budgetCalories - consumedCalories + exerciseCalories;
-  const surplusOrDeficit = remainingCalories < 0 ? 'Surplus' : 'Deficit';
+  const surplusOrDeficit = remainingCalories < 0 ? 'Superávit' : 'Déficit';
   output.innerHTML = `
-  <span class="${surplusOrDeficit.toLowerCase()}">${Math.abs(remainingCalories)} Calorie ${surplusOrDeficit}</span>
+  <span class="${surplusOrDeficit.toLowerCase()}">${surplusOrDeficit} de Calorías ${Math.abs(remainingCalories)}</span>
   <hr>
-  <p>${budgetCalories} Calories Budgeted</p>
-  <p>${consumedCalories} Calories Consumed</p>
-  <p>${exerciseCalories} Calories Burned</p>
+  <p>Calorías Presupuestadas ${budgetCalories}</p>
+  <p>Calorías Consumidas ${consumedCalories}</p>
+  <p>Calorías Quemadas ${exerciseCalories}</p>
   `;
 
   output.classList.remove('hide');
